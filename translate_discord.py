8cae14c9cede1d0e3e303b4033b07581292dd961
Winnie Woo <86391366+winniiew@users.noreply.github.com>
2022-06-06 01:45:19 -0700

    Add files via upload

---
+++ translate_discord.py
@@ -0,0 +1,65 @@
+import discord
+import googletrans
+#from keep_alive import keep_alive
+
+from dotenv import load_dotenv
+
+# Create a translator object
+translator = googletrans.Translator()
+
+# The discord client
+client = discord.Client()
+
+# The token that allows client to run the script
+# token = ...
+token = "OTgzMjE5NzMyNzEyOTMxMzM5.G54iI2.quAMZ8Us9Dmng0dq6XMpphgWlpLkSn9jP_WB_0"
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+# Notify on console that the discord bot is ready
+@client.event
+async def on_ready():
+    print("The bot is ready!")
+
+
+# Responds to commands
+@client.event
+async def on_message(message):
+    # Checks that the bot is not the one who sent a message
+    if message.author == client.user:
+        return
+
+    # Hello test command
+    if message.content.startswith("!hello"):
+        await message.channel.send("Hello! {}".format(message.author.mention))
+
+    # Command format:
+    # !t <converted language> <message>
+    if message.content.lower().startswith("!t "):
+        # Grab the message after the command
+        msg = message.content[3:]
+        converted_lang = msg.split(" ")
+        msg = msg[len(converted_lang[0]):]
+
+        if converted_lang[0] != "en":
+            # Directly converts English to specified language
+            translated_message = translator.translate(msg, dest=converted_lang[0])
+            await message.channel.send("{} -> {}".format(msg, translated_message.text))
+        else:
+            # Translates the language and converts it to English
+            translated_message = translator.translate(msg)
+            await message.channel.send("{} -> {}".format(msg, translated_message.text))
+
+#keep_alive()
+load_dotenv()
+client.run(token)
